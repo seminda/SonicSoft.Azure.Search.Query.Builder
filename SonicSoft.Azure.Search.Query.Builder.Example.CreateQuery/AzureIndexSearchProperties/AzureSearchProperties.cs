d35bbd4f51bd6d4881fdbfd6a47113cde8b35615
Seminda Rajapaksha <seminda@gmail.com>
2020-05-31 22:02:38 +1000

    Add example project (#12)
    Co-authored-by: Seminda Rajapaksha <srajapaksha2@agl.com.au>

---
+++ SonicSoft.Azure.Search.Query.Builder.Example.CreateQuery/AzureIndexSearchProperties/AzureSearchProperties.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using SonicSoft.Azure.Search.Query.Builder.Contracts.PropertyMapper;
+
+namespace SonicSoft.Azure.Search.Query.Builder.Example.CreateQuery.AzureIndexSearchProperties
+{
+    public class AzureSearchProperties:IAzureSearchProperties
+    {
+        public const string HotelName = "HotelName";
+        public const string Category = "Category";
+        public const string Tags = "Tags";
+        public const string StreetAddress = "StreetAddress";
+        public const string City = "City";
+        public const string PostalCode = "PostalCode";
+        public const string Country = "Country";
+        public const string Rooms = "Rooms";
+        public const string RoomType = "Type";
+        public const string RoomBaseRate = "BaseRate";
+
+        public AzureSearchProperties()
+        {
+            SearchPropertyMaps = new List<SearchPropertyMap>()
+            {
+                CreateSearchPropertyMap(HotelName, HotelName, HotelName),
+                CreateSearchPropertyMap(Category, Category, Category),
+                CreateSearchPropertyMap(Tags, Tags, Tags, true),
+                CreateSearchPropertyMap(StreetAddress, StreetAddress, $"Address/{StreetAddress}"),
+                CreateSearchPropertyMap(City, City, $"Address/{City}"),
+                CreateSearchPropertyMap(PostalCode, PostalCode, $"Address/{PostalCode}"),
+                CreateSearchPropertyMap(Country, Country, $"Address/{Country}"),
+                CreateSearchPropertyMap(Rooms, Rooms, Rooms, true),
+                CreateSearchPropertyMap(Rooms, RoomType, $"{RoomType}"),
+                CreateSearchPropertyMap(Rooms, RoomBaseRate, $"{RoomBaseRate}"),
+                CreateSearchPropertyMap(Rooms, Tags, $"{Tags}", true)
+            };
+        }
+
+        public List<SearchPropertyMap> SearchPropertyMaps { get; set; }
+
+        private SearchPropertyMap CreateSearchPropertyMap(string parent, string property, string azureSearchPropertyMap,
+            bool isArray = false)
+        {
+            return new SearchPropertyMap
+            {
+                ParentPropertyName = parent,
+                PropertyName = property,
+                AzureSearchPropertyMap = azureSearchPropertyMap,
+                IsArray = isArray
+            };
+        }
+    }
+}
---
+++ SonicSoft.Azure.Search.Query.Builder.Example.CreateQuery/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using SonicSoft.Azure.Search.Query.Builder.Contracts;
+using SonicSoft.Azure.Search.Query.Builder.Contracts.Configs;
+using SonicSoft.Azure.Search.Query.Builder.Contracts.PropertyMapper;
+using SonicSoft.Azure.Search.Query.Builder.Enums;
+using SonicSoft.Azure.Search.Query.Builder.Example.CreateQuery.AzureIndexSearchProperties;
+using SonicSoft.Azure.Search.Query.Builder.QueryBuilder;
+
+
+namespace SonicSoft.Azure.Search.Query.Builder.Example.CreateQuery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Step 1: Create your search properties
+            var propertyMaps = new AzureSearchProperties();
+
+            //Step 2: Provide configurations required - Date format and Delimiter 
+            var searchConfig = new SearchConfiguration("yyyy-MM-dd", "|");
+
+            //Step 3: Create property mapper object
+            var filterMapper = new PropertyMapper(propertyMaps);
+
+            //Step 4: Create SearchQueryBuilder object
+            var searchQueryBuilder =new SearchQueryBuilder(filterMapper,searchConfig);
+
+            //Example Query we required to build
+            //  "((Rooms/any(x: x/Type ne 'Standard Room' and x/BaseRate ge 100 and search.in(x/Tags, 'jacuzzi tub|bathroom shower','|')) and search.in(Tags, 'view|laundry service', '|')) or Address/Country eq 'USA')"
+
+            //To create above query you have to build your searchQueryParameters in c#
+
+            //Build Rooms and tags query block
+            var roomsAndTagFilters = new List<SearchQueryParameter>
+            {
+                new SearchQueryParameter()
+                {
+                    Parent = AzureSearchProperties.Rooms,
+                    Name = AzureSearchProperties.Rooms,
+                    Value = "",
+                    ODataOperator = ODataOperators.Any,
+                    SubQueryParameterQueryOperators = QueryOperators.And,
+                    SubQueryParameters = new List<SearchSubQueryParameter>
+                    {
+                        new SearchSubQueryParameter
+                        {
+                            AdditionalFilterParent = AzureSearchProperties.Rooms,
+                            AdditionalFilterName = AzureSearchProperties.RoomType,
+                            Value = "Standard Room",
+                            ODataOperator = ODataOperators.Ne
+                        },
+                        new SearchSubQueryParameter
+                        {
+                            AdditionalFilterParent = AzureSearchProperties.Rooms,
+                            AdditionalFilterName = AzureSearchProperties.RoomBaseRate,
+                            Value = 100,
+                            ODataOperator = ODataOperators.Ge
+                        },
+                        new SearchSubQueryParameter
+                        {
+                            AdditionalFilterParent = AzureSearchProperties.Rooms,
+                            AdditionalFilterName = AzureSearchProperties.Tags,
+                            Value = "jacuzzi tub|bathroom shower",
+                            ODataOperator = ODataOperators.SearchIn
+                        }
+                    }
+                },
+                new SearchQueryParameter()
+                {
+                    Parent = AzureSearchProperties.Tags,
+                    Name = AzureSearchProperties.Tags,
+                    Value = "view|laundry service",
+                    ODataOperator = ODataOperators.SearchIn
+                }
+            };
+
+            //This parameter will build the "((Rooms/any(x: x/Type ne 'Standard Room' and x/BaseRate ge 100 and search.in(x/Tags, 'jacuzzi tub|bathroom shower','|')) and search.in(Tags, 'view|laundry service', '|'))"
+            var roomsAndTagQueryParameter = new SearchQueryParameters(roomsAndTagFilters,QueryOperators.And);
+
+            //This parameter will build the "Address/Country eq 'USA'"
+            var countryQueryParameter = new SearchQueryParameters(new List<SearchQueryParameter>
+            {
+                new SearchQueryParameter
+                {
+                    Parent = AzureSearchProperties.Country,
+                    Name = AzureSearchProperties.Country,
+                    Value = "USA",
+                    ODataOperator = ODataOperators.Eq
+                }
+            });
+
+            //Finally combined the two parameters with or gate
+
+            var azureSearchQuery =
+                searchQueryBuilder.BuildQuery(QueryOperators.Or, roomsAndTagQueryParameter, countryQueryParameter);
+
+            Console.WriteLine("Final Query:");
+            Console.WriteLine(azureSearchQuery);
+            Console.Read();
+        }
+    }
+}
